6038988002261aa0ef88027a4db9d323dfac7cd5
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 09:42:21 +0100

    docs: Documentado Variable mensaje en variables_basicas.py

--- 01_intro/autonomos/variables_basicas.py
+++ 01_intro/autonomos/variables_basicas.py
@@ -3,6 +3,8 @@ nombre = "David"
 
 # Variable ciudad
 ciudad = "Asturias"
+
+# Variable mensaje que combina 'nombre' y 'ciudad' en un string
 mensaje = f"{nombre} vive en {ciudad}"
 print(mensaje)
 
