29952e2a6b2cd419b3a91e2637c6d347a3339a70
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 12:32:28 +0100

    docs: descripcion bigotes_felices.py

---
+++ 01_intro/desafio/bigotes_felices.py
@@ -0,0 +1,4 @@
+# Desafio: Bigotes Felices
+# En el refugio Bigotes Felices quieren un script que pida el nombre del gato y su edad, 
+# y muestre: `Gato: <nombre> (edad: <edad>)`. 
+# Si la edad no es un número, muestra `Edad inválida`. 
