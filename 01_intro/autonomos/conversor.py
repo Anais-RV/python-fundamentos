5a1cd1facc571a964fe3160e6c1f7869f3f24163
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 10:15:03 +0100

    feat: conversion de numero_conv de tipo string a tipo float en conversor.py

--- 01_intro/autonomos/conversor.py
+++ 01_intro/autonomos/conversor.py
@@ -2,3 +2,5 @@ numero_conv = input("Introduce un número a convertirlo en float")
 
 print(f"Número introducido: {numero_conv}")
 print(f"Tipo de dato: {type(numero_conv)}")
+
+numero_conv = float(numero_conv)
