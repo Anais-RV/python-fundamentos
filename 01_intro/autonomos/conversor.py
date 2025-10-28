3aa5230c6176f2487cdb49998d5652776221319c
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 10:31:38 +0100

    feat: imprimir resultado numero_conv de tipo float en conversor.py

--- 01_intro/autonomos/conversor.py
+++ 01_intro/autonomos/conversor.py
@@ -4,3 +4,4 @@ print(f"Número introducido: {numero_conv}")
 print(f"Tipo de dato: {type(numero_conv)}")
 
 numero_conv = float(numero_conv)
+print(f"Perfecto! Se ha convertido {numero_conv} en un valor float: {type(numero_conv)}")
