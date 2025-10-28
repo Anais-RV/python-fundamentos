51b5bff77771d5e96a04e19f538c449045b79dc3
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 12:34:37 +0100

    feat: declaracion variable 'nombre_gato' a pedir datos en bigotes_felices.py

--- 01_intro/desafio/bigotes_felices.py
+++ 01_intro/desafio/bigotes_felices.py
@@ -2,3 +2,5 @@
 # En el refugio Bigotes Felices quieren un script que pida el nombre del gato y su edad, 
 # y muestre: `Gato: <nombre> (edad: <edad>)`. 
 # Si la edad no es un número, muestra `Edad inválida`. 
+
+nombre_gato = input("Nombre del gato --> ")
