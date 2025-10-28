30b776ea61867addc4bd1678c2b01e300b58a1a9
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 11:21:39 +0100

    fix: texto numero_conv corregido

--- 01_intro/autonomos/conversor.py
+++ 01_intro/autonomos/conversor.py
@@ -1,5 +1,5 @@
 # Variable a introducir un dato
-numero_conv = input("Introduce un número a convertirlo en float")
+numero_conv = input("Introduce un número a convertirlo en float --> ")
 
 print(f"Número introducido: {numero_conv}")
 print(f"Tipo de dato: {type(numero_conv)}")
