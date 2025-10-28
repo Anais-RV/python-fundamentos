96b5634e09e3c837126d6cff5aee41f41c2010c7
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 11:17:12 +0100

    refactor: estructuración con try except para que no vuelva a salir ValueError en conversor.py

--- 01_intro/autonomos/conversor.py
+++ 01_intro/autonomos/conversor.py
@@ -3,8 +3,11 @@ numero_conv = input("Introduce un número a convertirlo en float")
 print(f"Número introducido: {numero_conv}")
 print(f"Tipo de dato: {type(numero_conv)}")
 
-numero_conv = float(numero_conv)
-print(f"Perfecto! Se ha convertido {numero_conv} en un valor float: {type(numero_conv)}")
+try:
+    numero_conv = float(numero_conv)
+    print(f"Perfecto! Se ha convertido {numero_conv} en un valor float: {type(numero_conv)}")
+except ValueError:
+    print(f"Lo siento! '{numero_conv}' no es un número válido. Por favor, vuelva a ejecutar de nuevo el archivo")
 
 # Conversion valor 45 convertido a float con éxito ✅
 
