b652eef1d08f1e8b657227107ad55857ee048525
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-28 09:44:10 +0100

    docs: Documentado imprimir variable mensaje en variables_basicas.py

--- 01_intro/autonomos/variables_basicas.py
+++ 01_intro/autonomos/variables_basicas.py
@@ -6,6 +6,8 @@ ciudad = "Asturias"
 
 # Variable mensaje que combina 'nombre' y 'ciudad' en un string
 mensaje = f"{nombre} vive en {ciudad}"
+
+# Imprimir variable mensaje
 print(mensaje)
 
 # Prueba 'python variables_basicas' Funciona ✅ 
