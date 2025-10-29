8726e17f6b243adf8fc0516888265749cbed978a
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-29 13:57:38 +0100

    docs: enunciado casting.py en 02_estructuras

---
+++ 02_estructuras/casting/casting.py
@@ -0,0 +1,4 @@
+# EJERCICIO GUIADO 2: CASTING Y VALICADIÓN
+# Paso 1: Crea `casting.py` que pida un número con `input`.
+# Paso 2: Convierte a `int` dentro de `try/except`.
+# Paso 3: Si falla, imprime `Entrada inválida`.
