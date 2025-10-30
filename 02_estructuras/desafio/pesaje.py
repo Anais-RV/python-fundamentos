19a1c0de8510e3d2e2c1a26af7c57926990e3b2f
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 21:05:28 +0100

    feat: try/except en 'pesaje.py'

--- 02_estructuras/desafio/pesaje.py
+++ 02_estructuras/desafio/pesaje.py
@@ -6,17 +6,21 @@ gato1 = input("Introduce el peso del primer gato: ")
 gato2 = input("Introduce el peso del segundo gato: ")
 gato3 = input("Introduce el peso del tercer gato: ")
 
-gato1 = int(gato1)
-gato2 = int(gato2)
-gato3 = int(gato3)
+try: 
+    gato1 = int(gato1)
+    gato2 = int(gato2)
+    gato3 = int(gato3)
 
-promedio = (gato1 + gato2 + gato3) / 3
+    promedio = (gato1 + gato2 + gato3) / 3
 
-print(f"Pesaje de los gatos: ")
-print(f"Gato nº1: {gato1} kg")
-print(f"Gato nº2: {gato2} kg")
-print(f"Gato nº3: {gato3} kg")
-print(f"Calculo promedio de los 3 gatos: {promedio}")
+    print(f"Pesaje de los gatos: ")
+    print(f"Gato nº1: {gato1} kg")
+    print(f"Gato nº2: {gato2} kg")
+    print(f"Gato nº3: {gato3} kg")
+    print(f"Calculo promedio de los 3 gatos: {promedio}")
+
+except ValueError:
+    print(f"Error al introducir los datos. Por favor, vuelva a iniciar el pesaje.")
 
 # Segundo test: introducir 'hola', 'que' y 'cachopo' respectivamente cuando le pida el programa
 # Resultado al ejecutar 'python pesaje.py':
