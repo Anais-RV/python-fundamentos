c376ec7d05f3518f46662327226ee81310fa02d4
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 20:49:23 +0100

    feat: declaracion variables gato1, gato2 y gato3 en 'pesaje.py'

--- 02_estructuras/desafio/pesaje.py
+++ 02_estructuras/desafio/pesaje.py
@@ -1,3 +1,7 @@
 # DESAFIO: Bigotes Felices (pesaje)
 # Enunciado: Pide el peso de tres gatos y calcula el promedio.
 # Valida entradas numéricas. 
+
+gato1 = input("Introduce el peso del primer gato: ")
+gato2 = input("Introduce el peso del segundo gato: ")
+gato3 = input("Introduce el peso del tercer gato: ")
