a33c98aa440d6c43571314ca0fb42a3a694d11a9
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 19:47:09 +0100

    docs: variable num_redondo en 'redondo.py'

--- 02_estructuras/autonomos/redondeo.py
+++ 02_estructuras/autonomos/redondeo.py
@@ -4,6 +4,13 @@
 # Variable num con el número específico a redondear
 num = 3.14159
 
+# Variable num_redondo que redondea variable num a 3 decimales
+# round(): devuelve un número con decimales que es una versión redondeada del
+#          mismo, con el número especificado de decimales.
+# Sintaxis: round(number, digits)
+# number --> Obligatorio. El número a ser redondeado
+# digits --> Opcional. El número específico a usar para redondear 
+#            number. Por defecto es 0.
 num_redondo = round(num, 3)
 
 print(f"Número: {num}")
