55c79b4f60e980c01a5b031c8649693fdca9992c
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 09:21:47 +0100

    feat: imprime 'igualque' en booleans.py

--- 02_estructuras/autonomos/booleans.py
+++ 02_estructuras/autonomos/booleans.py
@@ -14,3 +14,4 @@ menorque = num1 < num2
 print(f"CONDICIONES SIMPLES:")
 print(f"{num1} >= {num2} : {mayorigualque}")
 print(f"{num1} <= {num2} : {menorigualque}")
+print(f"{num1} == {num2} : {igualque}")
