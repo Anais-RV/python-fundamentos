d1f955b3220d8e1ddb09d43f37add729ad8feede
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 14:04:26 +0100

    feat: convertir num1/num2 en enteros en booleans.py

--- 02_estructuras/autonomos/booleans.py
+++ 02_estructuras/autonomos/booleans.py
@@ -5,6 +5,9 @@
 num1 = input("Escribe un número: ")
 num2 = input("Escribe otro número: ")
 
+num1 = int(num1)
+num2 = int(num2)
+
 # Declaramos variables de las condiciones simples:
 # Condición si el primer número es Mayor o igual que el segundo número
 mayorigualque = num1 >= num2
@@ -21,14 +24,15 @@ menorque = num1 < num2
 
 # Imprimir las condiciones simples en consola
 print(f"CONDICIONES SIMPLES:")
-print(f"{num1} >= {num2} : {mayorigualque}")
+print(f"{num1} >= {num2} : {num1 + num2}")
 print(f"{num1} <= {num2} : {menorigualque}")
 print(f"{num1} == {num2} : {igualque}")
 print(f"{num1} != {num2} : {distintoque}")
 print(f"{num1} > {num2} : {mayorque}")
 print(f"{num1} < {num2} : {menorque}")
 
-# RESULTADO ESPERADO con comando 'python booleans.py':
+# Primer test: introducir '4' como num1 y '6' como num2. 
+# Resultado:
 # CONDICIONES SIMPLES:
 # 4 >= 6 : False
 # 4 <= 6 : True
