4b4140856d6536678f6ccf18bf21846b4b4398bf
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 19:37:27 +0100

    test: test de ejecución 'redondo.py'; sale bien

--- 02_estructuras/autonomos/redondeo.py
+++ 02_estructuras/autonomos/redondeo.py
@@ -7,3 +7,9 @@ num_redondo = round(num, 3)
 
 print(f"Número: {num}")
 print(f"Número redondeado a 3 decimales: {num_redondo}")
+
+# Test: ejecutar 'python redondeo.py
+# Resultado esperado:
+# Número: 3.14159
+# Número redondeado a 3 decimales: 3.142
+# Éxito ✅ 
