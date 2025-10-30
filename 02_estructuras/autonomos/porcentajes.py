07990bf88e6469ccc1c98f384b3ed4ff15089e98
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 20:14:06 +0100

    test: primer test en 'porcentajes.py'

--- 02_estructuras/autonomos/porcentajes.py
+++ 02_estructuras/autonomos/porcentajes.py
@@ -10,3 +10,11 @@ porcentaje = 0.15
 resultado = num * porcentaje
 
 print(f"15% de {num} --> {resultado}")
+
+# Primer test: introducir '20' como num cuando lo pide el programa
+# Resultado al ejecutar 'python porcentajes.py':
+# --------------------------
+# Introduce un número: 20
+# 15% de 20 --> 3.0 
+# --------------------------
+# Éxito ✅ 
