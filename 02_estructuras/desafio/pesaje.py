2d0dd6f458632b1e999d7a212395c28b195a355d
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 21:01:01 +0100

    test: primer test 'pesaje.py'

--- 02_estructuras/desafio/pesaje.py
+++ 02_estructuras/desafio/pesaje.py
@@ -17,3 +17,17 @@ print(f"Gato nº1: {gato1} kg")
 print(f"Gato nº2: {gato2} kg")
 print(f"Gato nº3: {gato3} kg")
 print(f"Calculo promedio de los 3 gatos: {promedio}")
+
+# Primer test: introducir '10', '20' y '30' respectivamente cuando le pida el programa
+# Resultado al ejecutar 'python pesaje.py':
+# ---------------------------------------
+# Introduce el peso del primer gato: 10
+# Introduce el peso del segundo gato: 20
+# Introduce el peso del tercer gato: 30
+# Pesaje de los gatos:
+# Gato nº1: 10 kg
+# Gato nº2: 20 kg
+# Gato nº3: 30 kg
+# Calculo promedio de los 3 gatos: 20.0 
+# ---------------------------------------
+# Éxito ✅ 
