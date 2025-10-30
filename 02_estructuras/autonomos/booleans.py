536390ceaba0a61531a7aed90182562e184130df
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-30 14:11:10 +0100

    feat: agregar try/except con mensaje error en 'booleans.py'

--- 02_estructuras/autonomos/booleans.py
+++ 02_estructuras/autonomos/booleans.py
@@ -5,31 +5,36 @@
 num1 = input("Escribe un número: ")
 num2 = input("Escribe otro número: ")
 
-num1 = int(num1)
-num2 = int(num2)
+try :
 
-# Declaramos variables de las condiciones simples:
-# Condición si el primer número es Mayor o igual que el segundo número
-mayorigualque = num1 >= num2
-# Condición si el primer número es Menor o igual que el segundo número
-menorigualque = num1 <= num2
-# Condición si el primer número es Igual que el segundo número
-igualque = num1 == num2
-# Condición si el primer número es Distinto que el segundo número
-distintoque = num1 != num2
-# Condición si el primer número es Mayor que el segundo número
-mayorque = num1 > num2
-# Condición si el primer número es Menor que el segundo número
-menorque = num1 < num2
+    num1 = int(num1)
+    num2 = int(num2)
 
-# Imprimir las condiciones simples en consola
-print(f"CONDICIONES SIMPLES:")
-print(f"{num1} >= {num2} : {mayorigualque}")
-print(f"{num1} <= {num2} : {menorigualque}")
-print(f"{num1} == {num2} : {igualque}")
-print(f"{num1} != {num2} : {distintoque}")
-print(f"{num1} > {num2} : {mayorque}")
-print(f"{num1} < {num2} : {menorque}")
+    # Declaramos variables de las condiciones simples:
+    # Condición si el primer número es Mayor o igual que el segundo número
+    mayorigualque = num1 >= num2
+    # Condición si el primer número es Menor o igual que el segundo número
+    menorigualque = num1 <= num2
+    # Condición si el primer número es Igual que el segundo número
+    igualque = num1 == num2
+    # Condición si el primer número es Distinto que el segundo número
+    distintoque = num1 != num2
+    # Condición si el primer número es Mayor que el segundo número
+    mayorque = num1 > num2
+    # Condición si el primer número es Menor que el segundo número
+    menorque = num1 < num2
+
+    # Imprimir las condiciones simples en consola
+    print(f"CONDICIONES SIMPLES:")
+    print(f"{num1} >= {num2} : {mayorigualque}")
+    print(f"{num1} <= {num2} : {menorigualque}")
+    print(f"{num1} == {num2} : {igualque}")
+    print(f"{num1} != {num2} : {distintoque}")
+    print(f"{num1} > {num2} : {mayorque}")
+    print(f"{num1} < {num2} : {menorque}")
+
+except ValueError:
+    print("Inválido. Inténtelo de nuevo.")
 
 # Segundo test: introducir 're' como num1 y 'do' como num2. 
 # Resultado:
