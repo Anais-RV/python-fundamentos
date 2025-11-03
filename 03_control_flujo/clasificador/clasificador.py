f3256e4a13c8c26567767d71a50f5863a617acd5
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-03 13:49:19 +0100

    feat: completar clasificador simple 03_control_flujo

--- 03_control_flujo/clasificador/clasificador.py
+++ 03_control_flujo/clasificador/clasificador.py
@@ -1,2 +1,22 @@
 # EJERCICIO GUIADO 1: Clasificador simple
 # Enunciado: Pide un número y clasifícalo como negativo, cero o positivo. Valida `ValueError`.
+
+# TODO 1: Pedir número al usuario
+num = input("Escribe un número --> ")
+
+# TODO 2: Evaluar si es un número usando try/except
+try:
+    # TODO 3: Intentar convertir num a float
+    num = float(num)
+
+    # TODO 4: clasificar num usando if/elif/else
+    if num < 0:
+        print("Negativo")
+    elif num == 0:
+        print("Cero")
+    else:
+        print("Positivo")
+
+# TODO 5: Mensaje de error si num no es un número
+except ValueError:
+    print(f"ERROR ❌. El número introducido no es un número.")
