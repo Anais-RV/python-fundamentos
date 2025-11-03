d5eec3be361ee56dc69d03f74d29474ee041ab5b
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-03 21:43:41 +0100

    feat: completar ejercicio autonomo 2 en 03_control_flujo

--- 03_control_flujo/autonomos/cuenta_letras.py
+++ 03_control_flujo/autonomos/cuenta_letras.py
@@ -1,2 +1,23 @@
 # EJERCICIO AUTÓNOMO 2: Contar letras
 # Enunciado: Cuenta letras en un string usando un bucle.
+
+# TODO 0: Imprimir título del programa
+print("---- Cuenta Letras ---- \n")
+
+# TODO 1: Pedir al usuario unas palabras
+string = input("Escribe unas palabras --> ")
+
+# TODO 2: Iniciar un contador para las letras
+count = 0
+
+# TODO 3: Iniciar bucle for para recorrer cada caracter del string
+print("Caracter --> Número")
+for i in string:
+    # TODO 4: Incrementar el contador en 1 cada caracter que recorre 
+    count += 1
+    # TODO 5: Imprimir indice y letra del string recorrido
+    print(f"{i} --> {count}")
+    
+
+# TODO 6: Imprimir total del contador
+print(f"Letras contadas: {count}")
