9f69de67779200275a7f6e209abad39e91f2f46a
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-04 10:15:45 +0100

    feat: completar ejercicio autonomo 4 en 03_control_flujo

--- 03_control_flujo/autonomos/contraseñas.py
+++ 03_control_flujo/autonomos/contraseñas.py
@@ -1,2 +1,30 @@
 # EJERCICIO AUTÓNOMO 4: Contraseñas
 # Enunciado: Pide contraseñas hasta que sea `gato123`.
+
+# TODO 0: Imprimir título del programa
+print("---- Contraseñas ----")
+
+# TODO 1: Empezar bucle while que pida contraseñas hasta introducir 'gato123'
+while True:
+    # TODO 2: Pedir contraseña al usuario
+    contrasena = input("\nIntroduce una contraseña ('gato123' para finalizar) --> ")
+    # TODO 3: Si se introduce la contraseña especificada se acaba el bucle
+    if contrasena == "gato123":
+        print("Fin contraseñas!!!")
+        break
+    # TODO 4: Si no se introduce la contraseña especificada se guardará la contraseña introducida y seguirá pidiendo la contraseña
+    else:
+        print(f"Contraseña {contrasena} agregada.")
+        continue
+
+# RESULTADO:
+# ---- Contraseñas ----
+#
+# Introduce una contraseña ('gato123' para finalizar) --> pepe
+# Contraseña pepe agregada.
+#
+# Introduce una contraseña ('gato123' para finalizar) --> leoniko
+# Contraseña leoniko agregada.
+# 
+# Introduce una contraseña ('gato123' para finalizar) --> gato123
+# Fin contraseñas!!!
