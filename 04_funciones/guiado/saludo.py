24f92cb8c3c9d43ad877faad159ec51da67a2940
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-05 21:26:04 +0100

    feat: completar saludo.py en 04_funciones

--- 04_funciones/guiado/saludo.py
+++ 04_funciones/guiado/saludo.py
@@ -1,2 +1,24 @@
 # Guiado 1: Función saludo
 # Crea `def saludar(nombre):` que retorne `Hola, <nombre>`.
+
+# TODO 0: Imprime el titulo de ejercicio
+print("--- Función Saludo ---")
+# TODO 1: Crearemos una función con 1 parámetro (nombre) para mostrar el saludo
+def saludar(nombre):
+    # TODO 2: Devuelve el saludo
+    return f"Hola, {nombre}"
+
+# TODO 2: Pide un nombre al usuario
+nombre = input("\nIntroduce un nombre --> ")
+
+# TODO 3: Punto de entrada del programa
+if __name__ == "__main__":
+    print(saludar(nombre))
+
+# Resultado:
+# --- Función Saludo ---
+#
+# Introduce un nombre --> Pedro
+# Hola, Pedro
+# -----------------------------
+# Éxito ✅
