bc086c55bf8581af73734079df2592c04f13918f
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-06 18:31:54 +0100

    feat: completar ejercicio autonomo 5 en 04_funciones

--- 04_funciones/autonomos/ejercicio5.py
+++ 04_funciones/autonomos/ejercicio5.py
@@ -1,2 +1,14 @@
 # EJERCICIO AUTÓNOMO 5: Leer edad
 # Enunciado: `leer_edad()` pide una edad por `input()`, valida `ValueError` y retorna un entero o `None`.
+
+def leer_edad():
+    print("--- Leer edad ---")
+    try:
+        edad = int(input("\nIntroduce tu edad --> "))
+
+        return f"\nEdad: {edad} años \n"
+    except ValueError:
+        return "None\n"
+
+if __name__ == "__main__":
+    print(leer_edad())
