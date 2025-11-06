3755fb5cf0802558778542b6cbdb619a5e6b9e3d
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-06 12:08:45 +0100

    feat: completar ejercicio autonomo 3 en 04_funciones

--- 04_funciones/autonomos/ejercicio3.py
+++ 04_funciones/autonomos/ejercicio3.py
@@ -1,3 +1,26 @@
 # EJERCICIO AUTÓNOMO 3: Formatear gato
 # Enunciado: `formatear_gato(nombre, edad)` -> string con f-string
 
+def formatear_gato(nombre, edad):
+    return f"Su gato se llama {nombre} y tiene {edad} años."
+
+if __name__ == "__main__":
+    print("--- Formatear Gato ---")
+
+    try:
+        nombre = str(input("\nIntroduce el nombre de su gato --> "))
+        edad = int(input("Introduce la edad de su gato --> "))
+
+        print(formatear_gato(nombre, edad))
+    
+    except ValueError:
+        print("\nEntrada inválida ❌")
+
+# EJEMPLO:
+# --- Formatear Gato ---
+# 
+# Introduce el nombre de su gato --> Pequeflus
+# Introduce la edad de su gato --> 10
+# Su gato se llama Pequeflus y tiene 10 años.
+# ---------------------------------------------
+# Éxito ✅
