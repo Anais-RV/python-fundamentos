043086430a9626b9e959993042458255b65881c3
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-07 12:05:15 +0100

    fix: ejercicio guiado 1 en 05_colecciones

--- 05_colecciones/guiado/lista_gatos.py
+++ 05_colecciones/guiado/lista_gatos.py
@@ -1,12 +1,8 @@
 # EJERCICIO GUIADO 1: Lista de gatos
 # ENUNCIADO: Crea una lista con 3 nombres y recórrela imprimiendo índices y valores.
 
-# TODO 1: Crear lista de gatos con su identificador 'nombre'
-lista_gatos = [
-    {"nombre": "Niko"},
-    {"nombre": "Michi"},
-    {"nombre": "Chachito"}
-]
+# TODO 1: Crear lista de gatos 
+lista_gatos = ["Niko", "Michi", "Chachito"]
 
 # TODO 2: Crear función principal
 def main():
@@ -15,7 +11,7 @@ def main():
 
     # TODO 2.2: Iterar sobre la lista con enumerate()
     for i, gato in enumerate(lista_gatos, 1):
-        print(f"{i}: {gato['nombre']}")
+        print(f"{i}: {gato}")
 
 # TODO 3: Punto de entrada del programa
 if __name__ == "__main__":
