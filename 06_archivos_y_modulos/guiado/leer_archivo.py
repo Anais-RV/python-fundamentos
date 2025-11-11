b4749fd4fd6e268b545e1e02e98c5d775b2a64c4
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-11 11:50:23 +0100

    feat: completar ejercicio guiado 2 en 06_archivos_y_modulo

--- 06_archivos_y_modulos/guiado/leer_archivo.py
+++ 06_archivos_y_modulos/guiado/leer_archivo.py
@@ -1,3 +1,20 @@
 # EJERCICIO GUIADO 2: Leer archivo
 # ENUNCIADO: Lee `escribir.txt` e imprime las líneas numeradas.
 
+ARCHIVOTXT = 'escribir.txt'
+
+try:
+    with open(ARCHIVOTXT, 'r') as archivo:
+        lineas = archivo.readlines()
+
+        print("\nContenido del archivo: ")
+
+        for numero, linea in enumerate(lineas, 1):
+            # Usamos .rstrip() para eliminar el salto de línea al final
+            print(f"{numero}. {linea.rstrip()}")
+        
+        print(f"\n--- Información del archivo ---")
+        print(f"Total de líneas: {len(lineas)}")
+
+except FileNotFoundError:
+    print("❌ No se encontró el archivo")
