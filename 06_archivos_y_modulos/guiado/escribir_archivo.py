8945e88d047a42f91946eeb8cda1ed2c71241c50
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-11 11:17:33 +0100

    feat: completar ejercicio guiado 1 en 06_archivos_y_modulos

--- 06_archivos_y_modulos/guiado/escribir_archivo.py
+++ 06_archivos_y_modulos/guiado/escribir_archivo.py
@@ -1,6 +1,38 @@
 # EJERCICIO GUIADO 1: Escribir archivo
 # ENUNCIADO: Crea `escribir.txt` con tres líneas.
 
-# Abrir un archivo para leer
-leer = open("escribir.txt", "r")
+ARCHIVOTXT = "escribir.txt"
+
+try:
+    with open(ARCHIVOTXT, "w") as archivo:
+
+        # Escribir contenido en el archivo
+        archivo.write("Hola, que tal?")
+        archivo.write("\nMe encantan los cachopos")
+        archivo.write("\nY también los chipis")
+
+        # Cerrar el archivo
+        archivo.close()
+    print("\n✅ Creado y escrito el archivo correctamente.")
+except Exception as e:
+    print("\n❌ Error al escribir en el archivo txt: {e}")
+
+try:
+    with open(ARCHIVOTXT, "r") as archivo:
+        
+        print("\nLeyendo archivo...")
+
+        # Leer el contenido del archivo
+        contenido = archivo.read()
+
+        # Imprimir el contenido
+        print(contenido)
+
+        # Cerrar el archivo
+        archivo.close()
+
+    print("\nArchivo leído correcto ✅")
+except FileNotFoundError:
+    print("❌ Archivo no encontrado")
+
 
